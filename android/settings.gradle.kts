a64dceb4953132af858fa0d7faab6452fba98bee
dr0n123 <andrew-ost@mail.ru>
2023-11-13 22:48:56 +0300

    [Android]: initial commit

---
+++ android/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Split and pay"
+include(":app")
