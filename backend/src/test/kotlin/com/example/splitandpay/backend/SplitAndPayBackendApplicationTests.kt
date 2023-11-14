b4c4b5700806f5241b6ddcad156c789f4fae93fd
vitekkor <v.korotkikh@just-ai.com>
2023-11-14 12:17:32 +0300

    [Backend]: add base infrastructure

---
+++ backend/src/test/kotlin/com/example/splitandpay/backend/SplitAndPayBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.splitandpay.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SplitAndPayBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
