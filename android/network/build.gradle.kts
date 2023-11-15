9aff1fddf726b0d4ae109b30eb36480781d2f9ca
Timofey Pletnev <t.pletnev@tinkoff.ru>
2023-11-15 16:55:40 +0300

    [Android]: fix api url & java version in network module

--- android/network/build.gradle.kts
+++ android/network/build.gradle.kts
@@ -1,4 +1,7 @@
-@Suppress("DSL_SCOPE_VIOLATION")
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+private val apiUrl: String = gradleLocalProperties(rootDir).getProperty("api.url")
+
 plugins {
     alias(libs.plugins.library)
     alias(libs.plugins.kotlin)
@@ -6,13 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.splitandpay.network"
-    compileSdk = 33
+    compileSdk = 34
     buildFeatures.buildConfig = true
 
     defaultConfig {
         minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        targetSdk = 34
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -26,7 +28,7 @@ android {
             buildConfigField(
                 type = "String",
                 name = "API_URL",
-                value = "\"https://\"" // TODO
+                value = "\"$apiUrl\""
             )
         }
         debug {
@@ -34,16 +36,16 @@ android {
             buildConfigField(
                 type = "String",
                 name = "API_URL",
-                value = "\"https://\"" // TODO
+                value = "\"$apiUrl\""
             )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
