5c2a643dc58cc4a781d9b808b426087d82945f42
Matvei Borisov <bme15122000@gmail.com>
2023-11-17 01:12:23 +0300

    [iOS] add file with api path

---
+++ ios/Split and pay/Split and pay/Services/APIURL.swift
@@ -0,0 +1,13 @@
+//
+//  APIURL.swift
+//  Split and pay
+//
+//  Created by Матвей Борисов on 16.11.2023.
+//
+
+import Foundation
+
+struct APIURL {
+	static let apiURL = "http://{place here server address}/api"
+}
+
